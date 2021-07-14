ac05531b39fad1c52831137b64385577ec90e641
18WH1A1237 <69300360+18WH1A1237@users.noreply.github.com>
2021-07-14 12:19:45 +0530

    Add files via upload

---
+++ SL LAB/Experiment6/exp6.rb
@@ -0,0 +1,15 @@
+=begin
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+=end
+x=10
+while x >= 1 do
+    if (x % 2 != 0)
+        puts "#{x}"
+    end
+    x = x - 1
+end
